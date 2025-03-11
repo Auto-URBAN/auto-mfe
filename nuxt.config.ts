617b6aa7ebeaa2b6d77b8078b4474cf0dd5007ae
ruan.felssner <ruan_felssner@hotmail.com>
2025-03-11 01:32:07 -0300

    fix: remove nuxt-echarts module from nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/icon', 'nuxt-echarts'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/icon'],
   runtimeConfig: {
     apiUrl: process.env.NUXT_API_URL,
   },
